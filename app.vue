9dbbbc2b38fa68bc2d6414a8197c058c0f09a777
fxy <fxy17710117315@163.com>
2024-10-21 21:40:34 +0800

    fix bug

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <div class="w-full min-h-screen bg-[url('/bg.jpg')] bg-cover bg-fixed bg-no-repeat bg-center">
     <div class="absolute inset-0 bg-black bg-opacity-30"></div>
     <div class="relative z-10">
+      <NuxtRouteAnnouncer />
       <CommonNavbar />
       <NuxtPage />
       <CommonSocialLinks />
